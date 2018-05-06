053cf10ef1c5fbb33873d03399a867b163fe71a5
Maatuq <mahmoudmatook.mm@gmail.com>
2018-05-06 04:00:44 +0200

    witout function defintion

--- w2s2p1.py
+++ w2s2p1.py
@@ -1,13 +1,9 @@
-def func(balance,annualInterestRate,monthlyPaymentRate):
-    mir=annualInterestRate/12.0
-    i=1
-    while i<13:
-        mmp=monthlyPaymentRate*balance
-        mup=balance-mmp
-        balance=mup+mir*mup
-        i=i+1
-    
-    print("Remaining balance: " + str(round(balance,2)))
-func(42,.2,.04)
 
-
+mir=annualInterestRate/12.0
+i=1
+while i<13:
+    mmp=monthlyPaymentRate*balance
+    mup=balance-mmp
+    balance=mup+mir*mup
+    i=i+1
+print("Remaining balance: " + str(round(balance,2)))
