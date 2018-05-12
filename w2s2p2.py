d2aa271722d8f252bc263e87a32f44e71922426f
Maatuq <mahmoudmatook.mm@gmail.com>
2018-05-12 05:07:01 +0200

    modified:   w2s2p2.py

--- w2s2p2.py
+++ w2s2p2.py
@@ -1,17 +1,13 @@
-def func(balance,annualInterestRate):
-    mir=annualInterestRate/12.0
-    initbalance=balance
-    mfmp=10.0
-    while True:
-       for i in range(1,12):
+mir=annualInterestRate/12.0
+initbalance=balance
+mfmp=10.0
+while True:
+       for i in range(1,13):
             mub=balance-mfmp
             balance=mub+mir*mub
-       if balance==initbalance:
+       if balance<=0:
             print("Lowest Payment: "+str(mfmp))
             break
        else:
             balance=initbalance
             mfmp=mfmp+10.0
-
-func(3329,.2)
-
